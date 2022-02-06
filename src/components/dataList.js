0d3745de732528269702d4753dc9f8ce2222c7b9
mustefaj <722477+mustefa@users.noreply.github.com>
2022-02-05 22:05:17 -0500

    Initialize supabse magic-link login
    Note: accounts can be created using this sign-in
    - this needs to be disabled via supabase dashboard
    - show log-out button once users are logged in

--- src/components/dataList.js
+++ src/components/dataList.js
@@ -1,20 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 
-const ListofData = styled.ul`
+const ListofData = styled.div`
   margin: 0px;
   padding: 20px;
 `;
 
 function DataList(props) {
-  // this does not work if props.data is not defined.
+  // reminder, the below does not work if props.data is not defined.
   // const data = props.data;
 
   // instead we check if data is passed in & otherwise set data to null.
+  // and then map items from data to li tags.
   const data = props.data ? props.data : null;
-
-  // map items from data to li tags.
-  const items = data?.map((item) => <li key={item}>{item}</li>);
+  const items = data?.map((item) => <span key={item}>{item}</span>);
 
   // return list if items is populated, otherwise return span.
   return <ListofData>{data ? items : <span>nothing to show</span>}</ListofData>;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import DataList from "./dataList";
+import Auth from "api/auth";
 
-const Nav = styled(DataList)`
+const Nav = styled.div`
   background: ${(props) => props.theme.primary};
 `;
 
 function Navigation() {
-  return <Nav />;
+  return (
+    <Nav>
+      <Auth />
+      <DataList />
+    </Nav>
+  );
 }
 
 export default Navigation;
