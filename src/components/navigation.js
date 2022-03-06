392b8368c1538f722a378ea677bd21560bf1f6f0
mustefaj <722477+mustefa@users.noreply.github.com>
2022-03-05 19:14:07 -0500

    Listen to state change on load
    This uses https://github.com/supabase/gotrue-js/issues/23#issuecomment-970293583 for a resolution.

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,14 +3,37 @@ import styled from "styled-components";
 import DataList from "./dataList";
 import Auth from "api/auth";
 import { supabase } from "api/supabaseClient";
+import { useEffect, useState } from "react";
 
 const Nav = styled.div`
   background: ${(props) => props.theme.primary};
 `;
 
 function Navigation() {
-  const session = supabase.auth.session();
-  // console.log("User = " + session?.user.email);
+  const [session, setSession] = useState("");
+  // const [user, setUser] = useState(session?.user);
+ 
+  // Q: Why does this loop? Why don't we just stop it?
+  // supabase.auth.onAuthStateChange(( session) => {
+  //   setSession(session);
+  //   console.log(session);
+  // });
+
+  useEffect(() => {
+    const session = supabase.auth.session();
+    setSession(session);
+    // setUser(session?.user ?? null);
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async (event, session) => {
+        setSession(session);
+        // setUser(session?.user ?? null);
+      }
+    );
+    return () => {
+      authListener.unsubscribe();
+    };
+  }, []);
+
 
   return (
     <Nav>
